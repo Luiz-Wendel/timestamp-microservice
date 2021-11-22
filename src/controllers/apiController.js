e44d13fefc043b8b6656c3dd128bff406ee3297f
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-22 15:00:41 -0300

    feat: add apiController.timestamp

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -2,10 +2,22 @@ module.exports = {
   timestamp: (request, response) => {
     const { date } = request.params;
 
+    let unix = Date.now();
+    let utc = new Date(unix).toUTCString();
+
     if (!date) {
-      return response.status(200).json({ date: Date.now() });
+      return response.status(200).json({ unix, utc });
     }
 
-    return response.status(200).json({ date });
+    const dateObject = new Date(
+      Number.isNaN(Date.parse(date))
+        ? Number(date)
+        : date,
+    );
+
+    unix = Date.parse(dateObject);
+    utc = dateObject.toUTCString();
+
+    return response.status(200).json({ unix, utc });
   },
 };

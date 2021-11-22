390b2277dab56a43279f422fe1cedc0f7d65d278
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-11-22 15:36:09 -0300

    feat: remove root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (_request, response) => {
-  response.status(200).json({ message: 'OK' });
-});
-
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
